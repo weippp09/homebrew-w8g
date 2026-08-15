e3e7e93f62eacd58e02ff57a0abbc95c685209a6
weippp09 <15026554562@163.com>
2026-08-15 14:42:41 +0800

    bump: v0.1.1

--- Formula/w8g.rb
+++ Formula/w8g.rb
@@ -1,11 +1,11 @@
 class W8g < Formula
   desc "Self hot update CLI for Flutter apps (w8g)"
   homepage "https://github.com/weippp09/w8g"
-  version "0.1.0"
+  version "0.1.1"
 
   # 用物理 CPU 架构判断（兼容 Intel 版 Homebrew 跑在 Apple Silicon 上的场景）
   if Hardware::CPU.physical_cpu_arm64?
-    url "https://github.com/weippp09/w8g/releases/download/v0.1.0/w8g-darwin-arm64.zip"
+    url "https://github.com/weippp09/w8g/releases/download/v0.1.1/w8g-darwin-arm64.zip"
     sha256 "6db379f5b4cb892779d4e0ee2ebcab6ad8d603339aad2764e72a8534df1e049d"
   else
     odie "w8g 目前仅提供 macOS arm64 二进制，请使用 Apple Silicon Mac 安装"
