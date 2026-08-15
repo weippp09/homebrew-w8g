13991f2cf53e7ba07c01ea9792351bc57452eb01
weippp09 <15026554562@163.com>
2026-08-15 14:37:34 +0800

    update: 同步 v0.1.0 重新编译产物的 SHA256

--- Formula/w8g.rb
+++ Formula/w8g.rb
@@ -6,7 +6,7 @@ class W8g < Formula
   # 用物理 CPU 架构判断（兼容 Intel 版 Homebrew 跑在 Apple Silicon 上的场景）
   if Hardware::CPU.physical_cpu_arm64?
     url "https://github.com/weippp09/w8g/releases/download/v0.1.0/w8g-darwin-arm64.zip"
-    sha256 "49036fc346e2d54655506691946ba72f29f3871e16ee6cbd09ab07067e132c3a"
+    sha256 "6db379f5b4cb892779d4e0ee2ebcab6ad8d603339aad2764e72a8534df1e049d"
   else
     odie "w8g 目前仅提供 macOS arm64 二进制，请使用 Apple Silicon Mac 安装"
   end
